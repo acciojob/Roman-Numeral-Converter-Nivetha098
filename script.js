9b8d3c21ab324f839e7b4007087a4c1b935bc159
Nivetha098 <45388667+Nivetha098@users.noreply.github.com>
2025-05-07 06:52:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function convertToRoman(num) {
-    const obj = [
-        ['M', 1000], 
-        ['CM', 900], 
-        ['D', 500], 
-        ['CD', 400],
-        ['C', 100], 
-        ['L', 50], 
-        ['XL', 40], // Add this for 40
-        ['X', 10], 
-        ['IX', 9],
-        ['V', 5], 
-        ['IV', 4],
-        ['I', 1]
-    ];
-
+   const obj = [
+    ['M', 1000], 
+    ['CM', 900], 
+    ['D', 500], 
+    ['CD', 400],
+    ['C', 100], 
+    ['L', 50], 
+    ['XL', 40], // Correctly added for 40
+    ['X', 10], 
+    ['IX', 9],
+    ['V', 5], 
+    ['IV', 4],
+    ['I', 1]
+];
     let result = '';
 
     // Loop through each symbol/value pair
