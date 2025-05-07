6341d73b27b41e9fd82d6570e368f75210b9a50d
Nivetha098 <45388667+Nivetha098@users.noreply.github.com>
2025-05-07 06:49:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function convertToRoman(num) {
-    function convertToRoman(num) {
     const obj = [
         ['M', 1000], 
         ['CM', 900], 
         ['D', 500], 
         ['CD', 400],
         ['C', 100], 
-        ['LX', 50], 
+        ['L', 50], 
+        ['XL', 40], // Add this for 40
         ['X', 10], 
         ['IX', 9],
         ['V', 5], 
@@ -30,29 +30,7 @@ function convertToRoman(num) {
     return result; // Return the final Roman numeral
 }
 
-// You can test your code by running the above function and printing it to console
-console.log(convertToRoman(36)); // Output: XXXVI
-console.log(convertToRoman(4)); // Output: IV
-console.log(convertToRoman(9)); // Output: IX
-console.log(convertToRoman(14)); // Output: XIV
-console.log(convertToRoman(798)); // Output: DCCXCVIII
-    let result = '';
-
-    // Loop through each symbol/value pair
-    for (let i = 0; i < obj.length; i++) {
-        const [symbol, value] = obj[i];
-
-        // While the number is greater than or equal to the value
-        while (num >= value) {
-            result += symbol; // Append the symbol
-            num -= value; // Reduce the number
-        }
-    }
-
-    return result; // Return the final Roman numeral
-}
-
-// You can test your code by running the above function and printing it to console
+// Test your code
 console.log(convertToRoman(36)); // Output: XXXVI
 console.log(convertToRoman(4)); // Output: IV
 console.log(convertToRoman(9)); // Output: IX
